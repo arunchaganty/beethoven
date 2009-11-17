dd3f1f3d5c67f8a26c5f272708e9807977ec6999
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2009-11-18 01:15:59 +0530

    Changed phenotype representation to [(note,duration)] form

--- MusicGene.py
+++ MusicGene.py
@@ -6,116 +6,54 @@ from mingus.containers import *
 from mingus.core import *
 from mingus.midi import fluidsynth
 
-def normalise_note(note):
-    return notes.int_to_note(int(note)%12)
+from evaluators import *
 
-def inner_pdt(x, y):
-    assert(len(x) == len(y))
-    wt = sum([x[i] * y[i] for i in xrange(len(x))])
-    return wt
-
-def vector_sum(x, y):
-    assert(len(x) == len(y))
-    return tuple([x[i] + y[i] for i in xrange(len(x))])
-
-def remove_duplicates(lst):
-    assert(len(lst) > 0)
-    lst.sort()
-    lst_ = []
-    x = lst[0]
-    lst_.append(x)
-    for i in lst:
-        if i == x: continue
-        x = i
-        lst_.append(x)
-
-    return lst_
-
-def merge_note_container(noteC):
-    key = [normalise_note(note) for note in noteC]
-    key = remove_duplicates(key)
-    if len(key) == 1: 
-        return Note(key[0])
-    chord = chords.determine(key, True, True, True)
-
-    if chord and notes.is_valid_note(chord[0]):
-        if len(chord) > 1 and (chord[1] == '#' or chord[1] == 'b'):
-            return Note(chord[:2])
-        else:
-            return Note(chord[0])
-    else: 
-        return None
-
-def range_evaluator(track):
-    min, max = track[0].get_range()
-    if max == -1: 
-        min, max = int(min), int(min)
-    else:
-        min, max = int(min), int(max)
+def convert_from_track(track):
+    """ Converts a mingus track to a more convenient notation """
+    assert(isinstance(track, Track))
+    track_ = []
     for bar in track:
-        min_, max_ = bar.get_range()
-        if max_ == -1: 
-            min_, max_ = int(min_), int(min_)
-        else:
-            min_, max_ = int(min_), int(max_)
-        if min_ < min: min = min_
-        if max_ > max: max = max_
+        for beat, duration, notes in bar:
+            assert(len(notes) == 1)
+            track_.append((notes[0], duration))
 
-    return max - min
+    return track_
 
-# TODO: Should be a state machine that keeps track of the last (say) 2 - 3 notes
-def contour_evaluator(track):
-    # Check difference between first and last notes to get the "direction" of
-    # the bar
+def convert_to_track(track):
+    """ Convert our notation of a track to a mingus one """
+    assert(not isinstance(track, Track))
+    track_ = Track()
+    for note, duration in track:
+        track_.add_notes([note], duration)
 
-    wt = (0.4, 0.5, 0.2)
-
-    contour = (0, 0, 0)
-
-    dir = 0
-    note = Note()
-    for bar in track:
-        for beat_, duration_, note_ in bar:
-            if not note_: continue
-            note_ = merge_note_container(note_)
-            if not note_: continue
-            dir_ = int(note_) - int(note)
-
-            if (dir > 0 and dir_ > 0) or (dir < 0 and dir_ < 0): contour = vector_sum(contour,(1, 0, 0))
-            elif (dir < 0 and dir_ > 0) or (dir > 0 and dir_ < 0): contour = vector_sum(contour,(0, 1, 0))
-            elif dir_ == 0: contour = vector_sum(contour,(0, 0, 1))
-            dir = dir_
-    if sum(contour):
-        contour = tuple([value/sum(contour) for value in contour])
-
-    return inner_pdt(contour, wt)
+    return track_
 
 class MusicGene(Gene):
     def __init__(self, track):
-        self.track = track
+        if isinstance(track, Track):
+            self.track = convert_from_track(track)
+        else:
+            self.track = track
         Gene.__init__(self)
 
     def __str__(self):
         str_ = "MusicGene: " + "fitness " + str(self.fitness) + '\n' 
-        str_ += self.print_track(self.track)
+        str_ += str(self.track)
         return str_
 
     def __repr__(self):
         return str(self)
 
     def print_track(self, track):
-        str_ = ""
-        for bar in track:
-            for note in bar:
-                str_ += str(note[2]) + " "
-        return str_
+        return str(self.track)
 
     def get_fitness(self):
-        wt = (0.5, 1)
-        features = (range_evaluator(self.track), contour_evaluator(self.track))
+        #wt = (0.5, 1)
+        #features = (range_evaluator(self.track), contour_evaluator(self.track))
         #wt = (0.5,)
         #features = (range_evaluator(self.track),)
-        fitness = inner_pdt(wt, features)
+        #fitness = inner_pdt(wt, features)
+        fitness = 0.1
         return fitness
 
     def mate(self, other):
@@ -127,6 +65,35 @@ class MusicGene(Gene):
         else:
             return self.interleave_crossover(other)
 
+    def double_pt_crossover(self, other):
+        # Most naive method - interleave bars
+        #print "Mating:"
+        #print "P1: " + self.print_track(self.track)
+        #print "P2: " + self.print_track(other.track)
+        assert(len(self.track) == len(other.track))
+        children = []
+
+        crossover = random.randint(0,len(self.track))
+        crossover_ = random.randint(crossover,len(self.track))
+
+        child = []
+        child_ = []
+        for i in xrange(len(self.track)):
+            note = self.track[i]
+            note_ = other.track[i]
+
+            if i < crossover or i > crossover_:
+                child.append(note)
+                child_.append(note_)
+            else:
+                child.append(note_)
+                child_.append(note)
+
+        children += [MusicGene(child), MusicGene(child_)]
+
+        return children
+
+
     def single_pt_crossover(self, other):
         # Most naive method - interleave bars
         #print "Mating:"
@@ -135,27 +102,20 @@ class MusicGene(Gene):
         assert(len(self.track) == len(other.track))
         children = []
 
-        note_count = 0
-        for bar in self.track:
-            note_count += len(bar)
-        crossover = random.randint(0, note_count)
+        crossover = random.randint(0,len(self.track))
 
-        child = Track()
-        child_ = Track()
-        note_count = 0
+        child = []
+        child_ = []
         for i in xrange(len(self.track)):
-            bar = self.track[i]
-            bar_ = other.track[i]
-            for j in xrange(len(bar)):
-                beat, duration, notes = bar[j]
-                beat_, duration_, notes_ = bar_[j]
-                note_count+=1
-                if note_count < crossover:
-                    child.add_notes(notes, duration)
-                    child_.add_notes(notes_, duration_)
-                else:
-                    child.add_notes(notes_, duration_)
-                    child_.add_notes(notes, duration)
+            note = self.track[i]
+            note_ = other.track[i]
+
+            if i < crossover:
+                child.append(note)
+                child_.append(note_)
+            else:
+                child.append(note_)
+                child_.append(note)
 
         children += [MusicGene(child), MusicGene(child_)]
 
@@ -166,31 +126,26 @@ class MusicGene(Gene):
 
         children = []
 
-        child = Track()
-        child_ = Track()
-
-        note_count = 0
+        child = []
+        child_ = []
         for i in xrange(len(self.track)):
-            bar = self.track[i]
-            bar_ = other.track[i]
-            for j in xrange(len(bar)):
-                beat, duration, notes = bar[j]
-                beat_, duration_, notes_ = bar_[j]
-                note_count+=1
-                if note_count%2:
-                    child.add_notes(notes, duration)
-                    child_.add_notes(notes_, duration_)
-                else:
-                    child.add_notes(notes_, duration_)
-                    child_.add_notes(notes, duration)
+            note = self.track[i]
+            note_ = other.track[i]
+
+            if i%2 == 0:
+                child.append(note)
+                child_.append(note_)
+            else:
+                child.append(note_)
+                child_.append(note)
 
         children += [MusicGene(child), MusicGene(child_)]
-
         return children
 
     def mutate(self):
         return self
 
     def play(self):
-        fluidsynth.play_Track(self.track)
+        track = convert_to_track(self.track)
+        fluidsynth.play_Track(track)
 
